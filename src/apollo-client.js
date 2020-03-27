a13167c78c42aecd8b5845a4174b997e58b96b63
endorphin82 <endorphin82@gmail.com>
2020-03-27 22:04:53 +0200

    init

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -2,4 +2,5 @@ import ApolloClient from "apollo-boost"
 
 export const client = new ApolloClient({
   uri: 'http://localhost:3005/graphql'
+  // uri: 'https://restockchicago-server.herokuapp.com/graphql'
 })
