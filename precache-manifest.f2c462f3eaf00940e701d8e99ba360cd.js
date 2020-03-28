31de7d0b5c8c482b753ad05c43176453f824b301
endorphin82 <endorphin82@gmail.com>
2020-03-28 15:31:52 +0200

    Updates

---
+++ precache-manifest.f2c462f3eaf00940e701d8e99ba360cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d64ec694b2220e65f580b0bdb86152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65bb5700cabfc74468b",
+    "url": "/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "a4107573b8c578a76ad1",
+    "url": "/static/css/main.e2a777c1.chunk.css"
+  },
+  {
+    "revision": "b65bb5700cabfc74468b",
+    "url": "/static/js/2.4c80f025.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.4c80f025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4107573b8c578a76ad1",
+    "url": "/static/js/main.25c54f96.chunk.js"
+  },
+  {
+    "revision": "446e1c846f35bb5955ac",
+    "url": "/static/js/runtime-main.d6d949c1.js"
+  }
+]);
