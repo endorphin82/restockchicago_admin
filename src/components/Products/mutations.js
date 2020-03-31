5629f2874fcda98d75cf7c40729f017aefb1c8d1
endorphin82 <endorphin82@gmail.com>
2020-03-31 21:00:07 +0300

    edit product done

--- src/components/Products/mutations.js
+++ src/components/Products/mutations.js
@@ -3,7 +3,13 @@ import { gql } from "apollo-boost"
 export const addProductMutation = gql`
     mutation addProduct($name: String!, $price: Int!, $categoryId: ID!, $images: [String] ) {
         addProduct(name: $name, price: $price, categoryId: $categoryId, images: $images ){
+            id
             name
+            price
+            category{
+                id
+                name
+            }
         }
     }
 `
