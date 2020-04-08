1b4b71294dee9ce76afc37661ada2704eb847620
endorphin82 <endorphin82@gmail.com>
2020-04-08 14:11:50 +0300

    add ts reducers actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
-
 import { createStore, compose } from "redux";
 import { rootReducer } from "../reducer"
 
 const composeEnhancers =
   typeof window === "object" &&
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__({})
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
 
 const enhancer = composeEnhancers();
 
